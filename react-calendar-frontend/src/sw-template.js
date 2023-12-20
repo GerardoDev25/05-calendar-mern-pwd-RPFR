f9d6bffb038ca0f931c7e133297283f6c69e38f4
Gerardo Miranda <gerardodev25@gmail.com>
2023-12-20 18:49:40 -0400

    perf(pwa): 006 Workbox Cache Strategies
    006 Workbox Cache Strategies
    
    006 Workbox Cache Strategies

--- react-calendar-frontend/src/sw-template.js
+++ react-calendar-frontend/src/sw-template.js
@@ -1,6 +1,24 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-undef */
 importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
+const { registerRoute } = workbox.routing;
+const { CacheFirst } = workbox.strategies;
+
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  new RegExp(
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
+  ),
+  new CacheFirst()
+);
+
+registerRoute(
+  new RegExp(
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
+  ),
+  new CacheFirst()
+);
